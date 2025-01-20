451d9e09a66271b9a415c1adcb8e156b17763723
Javidan Ganbar <ganbarjavidan@gmail.com>
2025-01-21 01:27:33 +0400

    Adjusted R-Squared and Explained Variance Score added. Regression metrics are done for now.

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -95,3 +95,59 @@ def r2_score(y_true, y_pred):
     r2 = 1 - (rss / tss)
 
     return r2
+
+def adjr2_score(y_true, y_pred, n, k):
+
+    """
+    Calculate Adjusted R-Squared score.
+    :param y_true (numpy.ndarray): true target values
+    :param y_pred (numpy.ndarray): predicted target values
+    :param n: number of data points in test set
+    :param k: number of predictors
+    :return float: adjusted r-squared score.
+    """
+
+    # Ensure inputs are arrays and integer
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    n = int(n)
+    k = int(k)
+
+    # Calculate R-Squared (Coefficient of Determination)
+    rss = np.sum(np.square(y_true - y_pred))
+    tss = np.sum(np.square(y_true - np.mean(y_true)))
+    r2 = 1 - (rss / tss)
+
+    # Calculate Adjusted R-Squared score
+    r2_adj = 1 - ((1 - r2)*(n - 1)) / (n-k-1)
+
+    return r2_adj
+
+
+def explained_variance_score(y_true, y_pred):
+
+    """
+    Calculate Explained Variance Score (EVS).
+    :param y_true (numpy.ndarrays): true target values
+    :param y_pred (numpy.ndarrays): predicted target values
+    :return float: explained variance score
+    """
+
+    # Ensure inputs are arrays
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculate residuals
+    residuals = y_true - y_pred
+
+
+    # Calculate variance of residuals
+    var_residuals = np.var(residuals, ddof=1)
+
+    # Calculate total variance
+    total_var = np.var(y_true, ddof=1)
+
+    # Calculate Explained Variance Score (EVS)
+    evs = 1 - (var_residuals / total_var)
+
+    return evs
