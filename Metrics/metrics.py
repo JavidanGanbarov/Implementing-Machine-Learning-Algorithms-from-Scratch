04ef2f4f8fa8c2a40b030622cbfbdbf96e002a91
Javidan Ganbar <ganbarjavidan@gmail.com>
2025-01-20 21:26:38 +0400

    MSE, RMSE and R-Squared added.

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -12,12 +12,9 @@ def mean_absolute_error(y_true, y_pred):
 
     """
     Calculate Mean Absolute Error (MAE).
-    Parameters:
-        y_true (numpy.ndarray): true target values.
-        y_pred (numpy.ndarray): predicted target values.
-
-        Returns:
-            float: the mean absolute error
+    :param y_ture (numpy.ndarray): true target values.
+    :param y_pred (numpy.ndarray): predicted target values.
+    :return float: the mean absolute error.
     """
 
     # Ensure inputs are numpy arrays
@@ -32,5 +29,69 @@ def mean_absolute_error(y_true, y_pred):
 
     return mae
 
+def mean_squared_error(y_true, y_pred):
 
+    """
+    Calculate Mean Squared Error (MSE).
+    :param y_ture (numpy.ndarray): true target values.
+    :param y_pred (numpy.ndarray): predicted target values.
+    :return float: the mean squared error.
+    """
 
+    # Ensure inputs are NumPy arrays
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculate squared errors
+    squared_errors = np.square(y_true - y_pred)
+
+    # Calculate mean sqared error
+    mse = np.mean(squared_errors)
+
+    return mse
+
+def root_mean_squared_error(y_true, y_pred):
+
+    """
+    Calculate Root Mean Squared Error (RMSE).
+    :param y_true (numpy.ndarray): true target values.
+    :param y_pred (numpy.ndarray): predicted target values.
+    :return float: the root mean squared error.
+    """
+
+    # Ensure inputs are NumPy arrays
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculate squared errors
+    squared_errors = np.square(y_true - y_pred)
+
+    # Calculate mean squared error
+    mse = np.square(y_true - y_pred)
+
+    # Calculate root mean squared error
+    rmse = np.sqrt(mse)
+
+    return rmse
+
+def r2_score(y_true, y_pred):
+
+    """
+    Calculate R-squared (Coefficient of Determination)
+    :param y_true (numpy.ndarray): true target values.
+    :param y_pred (numpy.ndarray): predicted target values.
+    :return float: r-squared score.
+    """
+
+    # Ensure inputs are numpy arrays
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculate RSS and TSS
+    rss = np.sum(np.square((y_true - y_pred)))
+    tss = np.sum(np.square(y_true - np.mean(y_true)))
+
+    # Calculate r-squared score
+    r2 = 1 - (rss / tss)
+
+    return r2
