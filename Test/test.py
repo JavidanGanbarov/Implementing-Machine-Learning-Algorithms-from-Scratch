bb499083153dfc70d0c74ab9353071dba5fcbb4b
Javidan Ganbar <ganbarjavidan@gmail.com>
2025-01-24 14:27:24 +0400

    Test file modified.

--- Test/test.py
+++ Test/test.py
@@ -8,7 +8,6 @@ np.random.seed(42)
 X = np.random.rand(100, 1) * 10  # Feature values between 0 and 10
 y = 3 * X + np.random.randn(100, 1) * 2  # Linear relation with some noise
 
-# Split dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Simple Linear Regression model
@@ -18,13 +17,10 @@ model.fit(X_train, y_train)
 # Make predictions on the test set
 y_pred = model.predict(X_test)
 
-
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 print("Mean Squared Error: {}".format(mse))
 print("R^2 Score: {}".format(r2))
-
-
 
